d3966c7126b560b3622aa7d98e376308c92d1d76
piggy0713 <piggy.moomoo520@gmail.com>
2021-07-11 00:35:39 -0400

    stable version

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let result = document.getElementById("result");
 
 const inputDisplay = (num) => {
   const button = num.target.value;
+  const id = num.target.id;
 
   if (button === "reset") {
     input.innerHTML = 0;
   } else if (input.innerHTML === "0") {
-    input.innerHTML = button;
+    if (id === "numKey") {
+      input.innerHTML = button;
+    }
   } else if (button === "delete") {
-    if (input.innerHTML === "0") {
-      input.innerHTML = 0;
-    } else if (input.innerHTML.length === 1) {
+    if (input.innerHTML.length <= 1) {
       input.innerHTML = 0;
     } else {
       input.innerHTML = input.innerHTML.replace(/.$/, "");
@@ -23,8 +24,20 @@ const inputDisplay = (num) => {
 };
 
 const getResult = () => {
-  let resultInput = input.innerHTML;
-  input.innerHTML = Math.round(eval(resultInput) * 1000) / 1000;
+  const regex = /[+|-|*|/][0-9]/g;
+  let tempInput = input.innerHTML;
+  if (tempInput.includes(",")) {
+    tempInput = tempInput.replace(/,/g, "");
+  }
+  if (tempInput.match(regex)) {
+    let result = eval(tempInput);
+
+    let roundedResult = Math.round(result * 100) / 100;
+
+    let formatedResult = roundedResult.toLocaleString("en-US");
+
+    input.innerHTML = formatedResult;
+  }
 };
 
 userInput.addEventListener("click", inputDisplay);
